d394c592a40da871e615d2e39496a6cee093d0ad
vheon <v.vheon@gmail.com>
2012-03-26 16:44:20 +0200

    used MAP function

--- 20120326/script.js
+++ 20120326/script.js
@@ -1,4 +1,13 @@
-var domain1d = DOMAIN([[2,6]])([8]);
+var domain1d = DOMAIN([[0,1]])([10]);
 var domain2d = DOMAIN([[2,6],[2,8]])([8,12]); 
 var domain3d = DOMAIN([[2,6],[2,8],[2,8]])([8,12,12]);
+
+
+var mapping = function (p) {
+	var u = p[0];
+
+	return [u, 1];
+};
+
+var mapped = MAP(mapping)(domain1d);
 

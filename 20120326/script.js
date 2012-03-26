98c84f58dfb7b518b4ec5bcfcdad56ba8336bf1e
vheon <v.vheon@gmail.com>
2012-03-26 16:51:05 +0200

    used MAP again

--- 20120326/script.js
+++ 20120326/script.js
@@ -1,4 +1,4 @@
-var domain1d = DOMAIN([[0,1]])([10]);
+var domain1d = DOMAIN([[0,10]])([10]);
 var domain2d = DOMAIN([[2,6],[2,8]])([8,12]); 
 var domain3d = DOMAIN([[2,6],[2,8],[2,8]])([8,12,12]);
 
@@ -9,5 +9,12 @@ var mapping = function (p) {
 	return [u, 1];
 };
 
+var bisect = function (p) {
+	var u = p[0];
+
+	return [u, u];
+};
+
 var mapped = MAP(mapping)(domain1d);
+var bisettrice = MAP(bisect)(domain1d);
 
